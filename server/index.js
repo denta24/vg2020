6cc7a2afe851d66549fbd26214c2f0a6660f8c32
Dawid Kurpiel <kurpieldawid@gmail.com>
2020-03-12 20:00:49 +0100

    serverfix1

--- server/index.js
+++ server/index.js
@@ -2,65 +2,81 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-
 var fs = require("fs");
 const multer = require("multer");
-
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
 const config = require("./config/key");
 
 // const mongoose = require("mongoose");
-// mongoose
-//   .connect(config.mongoURI, { useNewUrlParser: true })
-//   .then(() => console.log("DB connected"))
-//   .catch(err => console.error(err));
-
-const mongoose = require("mongoose");
-const connect = mongoose
-  .connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
+// const connect = mongoose
+//   .connect(config.mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   })
+//   .then(() => console.log("MongoDB Connected..."))
+//   .catch(err => console.log(err));
 
 app.use(cors());
 
-//to not get any deprecation warning or error
-//support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
-//to get json data
-// support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 app.use("/api/users", require("./routes/users"));
-
-//use this to show the image you have in node js server to client (react js)
-//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use("/uploads", express.static("uploads"));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+//////////////////
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  // "mongodb+srv://local:dawidrw123@cluster0-ehgqo.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://local:dawidrw123@cluster0-ehgqo.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true";
+/////////////////////////////////////
 
-  // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+MongoClient.connect(
+  uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, db) => {
+    console.log("connected to MongoClient");
 
-const port = process.env.PORT || 5000;
+    app.get("/api/products/wybor", (req, res) => {
+      const collection = db.db("VellutoGiorno").collection("WybierzPrzedmioty");
 
-app.listen(port, () => {
-  console.log(`Server Running at ${port}`);
-});
+      collection
+        .find({})
+        .toArray()
+        .then(response => {
+          res.json({
+            response
+          });
+          console.log(response);
+        });
+    });
 
+    app.get("/api/product/:id?", (req, res) => {
+      const collection = db.db("VellutoGiorno").collection("test");
 
+      collection.findOne({ id: req.params.id * 1 }).then(response => {
+        res.json(response);
+      });
+    });
 
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static("client/build"));
+      app.get("*", (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname, "../client", "build", "index.html")
+        );
+      });
+    }
 
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server Running at ${port}`);
+    });
+  }
+);
