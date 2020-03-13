97671514e54503850cd1d36f3671e7c024d2eeeb
Dawid Kurpiel <kurpieldawid@gmail.com>
2020-03-14 00:13:30 +0100

    styling  fix

--- client/src/Components/Item.js
+++ client/src/Components/Item.js
@@ -136,7 +136,7 @@ export default class Item extends Component {
             <img
               src={item}
               alt=""
-              className="product__src"
+              className="product__src img-responsive"
               onMouseOver={this.handleMiniImgClick}
             />
           </div>
